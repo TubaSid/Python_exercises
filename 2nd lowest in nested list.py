49f60a97301d994d01337c502b81b494d3ece22b
Tuba Siddiqui <57428557+TubaSid@users.noreply.github.com>
2022-02-11 22:51:40 +0500

    Update 2nd lowest in nested list.py
    improved

--- 2nd lowest in nested list.py
+++ 2nd lowest in nested list.py
@@ -1,41 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  9 18:31:15 2021
-
-@author: teesid
-"""
-
 l=0
-
 idx=0
 index=0
 sort=[]
 nested_list=[]
-for i in range(int(input())):
-        name=(input())
-        score=(float(input()))
+for i in range(int(input("Enter no. of students: "))):
+        name=(input("Enter name: "))
+        score=(float(input("Enter score:")))
         nested_list.append([name,score])
 n=len(nested_list)
 for i in range(n):
     for j in range(i):
         if nested_list[i][1]>nested_list[j][1]:
-            print(nested_list[i][1], nested_list[j][1])
             c=nested_list[i]
             nested_list[i]=nested_list[j]
             nested_list[j]=c
-            
-print(nested_list)
-print("kkk")
-for i in range(n):
-    for j in range(i):
-        print(nested_list[i][1],nested_list[j][1])
-
-print(nested_list)
 nested_list.reverse()
-print(nested_list)
-#print(score)
-
-print(nested_list)
 for i in range(n):
     for j in range(i):
         if l<1:
@@ -45,18 +24,11 @@ for i in range(n):
                 v=nested_list[i][1];
         if nested_list[idx][1]==nested_list[i][1]:
             index=i
-print(idx,index)    
-"""if index<idx:
-    p=idx
-    idx=index
-    index=p"""
-#print(nested_list[1][1])
 if nested_list[idx][1]==nested_list[index][1]:
     for i in range(idx,index+1):
         sort.append(str(nested_list[i][0]))
 else:
     print(nested_list[idx][0])
-    #print("bbb")
 sort.sort()
 for i in range(len(sort)):
-    print(sort[i])
+    print(sort[i],"has the second lowest marks.")
